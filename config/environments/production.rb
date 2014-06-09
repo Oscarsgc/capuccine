9c575793a1d6ebe825c55661f6a50b967af88d03
Oscar Sergio Gonzalez Camacho <oscar_sgc@hotmail.com>
2014-06-09 17:28:48 -0400

    YUI

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,7 +72,6 @@ Spa::Application.configure do
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
   config.i18n.fallbacks = true
-  config.assets.css_compressor = :yui
   config.assets.js_compressor = :uglifier
 
   # Send deprecation notices to registered listeners.
